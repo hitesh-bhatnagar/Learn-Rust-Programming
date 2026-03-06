ed1c80d199e6021911c45299e287b89fb9b0424d
Hitesh Bhatnagar <125803878+hitesh-bhatnagar@users.noreply.github.com>
2026-03-06 12:13:29 +0530

    Added files

---
+++ examples/01_variables.rs
@@ -0,0 +1,12 @@
+fn main() {
+    
+    // Part 1 : Immutable vs Mutable Variables
+    let x = 5;
+    let mut y = 10;
+    y = 20 ;
+    
+    println!("x = {} ", x);
+    println!("y = {} ", y);
+    
+    // 
+}
