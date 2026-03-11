1235a11159756bee2f15c381a869b3570325ab88
Hitesh Bhatnagar <125803878+hitesh-bhatnagar@users.noreply.github.com>
2026-03-11 12:16:11 +0530

    functions, loops & user input

--- examples/01_variables.rs
+++ examples/01_variables.rs
@@ -50,8 +50,8 @@ fn main() {
     
     // PART5 : Constants
     
-    const euler_num: f32 = 2.71828;
-    println!("Max euler_num= {}", euler_num );
+    const EULER_NUM: f32 = 2.71828;
+    println!("Max euler_num= {}", EULER_NUM );
     
     
     
